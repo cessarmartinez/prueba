a5a79a6253bf62d0bda7a8a01d154d6961abad67
cessarmartinez <103976412+cessarmartinez@users.noreply.github.com>
2022-09-02 06:52:53 -0300

    -

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -3,7 +3,25 @@ import productos from "./productos"
 
 const Item = ({producto}) => {
     return (
-        <div>{producto.name}</div>
+        <>
+        <div className="col mb-5">
+            <div className="card h-100">
+                <img className="card-img-top" src={producto.imagen} alt="${product.descripcion}" />
+                <div className="card-body p-4">
+                    <div className="text-center">
+                        <h5 className="fw-bolder">{producto.name}</h5>
+                            {producto.precio}
+                        </div>
+                    </div>
+                    <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                        <div className="text-center">
+                            <button className="btn btn-outline-dark mt-auto " id="boton">Agregar
+                            </button>
+                        </div>
+                    </div>
+            </div>
+        </div>
+        </>
     )
 }
 
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -19,7 +19,13 @@ const ItemListContainer = (props) => {
         <div>
             <p>{titulo}</p>
             <p>{greeting}</p>
-            <ItemList listProducts={listProducts}/>
+            <section class="py-5">
+                <div class="container px-4 px-lg-5 mt-5">
+                    <div id="lista" class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+                        <ItemList listProducts={listProducts}/>
+                    </div>
+                </div>
+            </section>
         </div>
         </>
     )
